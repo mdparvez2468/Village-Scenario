794ff5db1dcd668aba6ef69e1a45defeb5576862
Bonna Akter <67730703+Bonna2585@users.noreply.github.com>
2022-06-20 11:59:38 +0600

    Update main.cpp
    update land function bonna akter committed

--- main.cpp
+++ main.cpp
@@ -13,6 +13,16 @@ int person = 0;
 void land()
 {
 
+	glColor3f(0.0 ,0.9 ,0.0);
+    glBegin(GL_POLYGON);
+
+    glVertex3i(0, 200, 0);
+    glVertex3i(1200, 200, 0);
+    glVertex3i(1200, 600, 0);
+    glVertex3i(0, 600, 0);
+
+
+glEnd();
 
 }
 void house()
@@ -20,11 +30,185 @@ void house()
 
 
 
+glColor3f(0.5, 0.5, 0.8);
+
+glBegin(GL_POLYGON);
+
+    glVertex3i(220, 350, 0);
+    glVertex3i(480, 350, 0);
+    glVertex3i(480, 500, 0);
+    glVertex3i(220, 500, 0);
+
+
+glEnd();
+
+glColor3f(1.0, 0.3, 1.0);
+glBegin(GL_POLYGON);
+
+    glVertex3i(200, 500, 0);
+    glVertex3i(500, 500, 0);
+    glVertex3i(350, 700, 0);
+
+
+
+glEnd();
+
+
+glColor3f(0.5, 0.4, 0.2);
+glBegin(GL_POLYGON);
+
+    glVertex3i(280, 350, 0);
+    glVertex3i(330, 350, 0);
+    glVertex3i(330, 460, 0);
+    glVertex3i(280, 460, 0);
+
+
+glEnd();
+
+glColor3f(0.5, 0.4, 0.2);
+glBegin(GL_POLYGON);
+
+    glVertex3i(280, 470, 0);
+    glVertex3i(330, 470, 0);
+    glVertex3i(330, 480, 0);
+    glVertex3i(280, 480, 0);
+
+
+glEnd();
+
+glColor3f(1,0,0);
+glBegin(GL_POLYGON);
+
+    glVertex3i(380, 465, 0);
+    glVertex3i(450, 465, 0);
+    glVertex3i(450, 485, 0);
+    glVertex3i(380, 485, 0);
+
+
+glEnd();
+
+glColor3f(1,0,0);
+glBegin(GL_POLYGON);
+
+    glVertex3i(380, 370, 0);
+    glVertex3i(450, 370, 0);
+    glVertex3i(450, 450, 0);
+    glVertex3i(380, 450, 0);
+
+
+glEnd();
+
+glColor3f(0.3,0.3,0.3);
+glBegin(GL_POLYGON);
+
+    glVertex3i(210, 330, 0);
+    glVertex3i(490, 330, 0);
+    glVertex3i(490, 350, 0);
+    glVertex3i(210, 350, 0);
+
+
+glEnd();
+
+glColor3f(0.7,0.7,0.7);
+glBegin(GL_POLYGON);
+
+    glVertex3i(275, 335, 0);
+    glVertex3i(335, 335, 0);
+    glVertex3i(335, 350, 0);
+    glVertex3i(275, 350, 0);
+
+
+glEnd();
+
+glColor3f(0.1,0.1,0.1);
+glBegin(GL_POLYGON);
+
+    glVertex3i(270, 320, 0);
+    glVertex3i(340, 320, 0);
+    glVertex3i(340, 335, 0);
+    glVertex3i(270, 335, 0);
+
+
+glEnd();
+
+
 
 }
 
 void kup()
 {
+
+
+ glColor3f(1,0,0);
+    glBegin(GL_POLYGON);
+
+
+    glVertex3i(90,380,0);
+    glVertex3i(130,380,0);
+    glVertex3i(130,260,0);
+    glVertex3i(90,260,0);
+    glEnd();
+
+
+    glColor3f(1,0,0);
+    glBegin(GL_POLYGON);
+    glVertex3i(75,360,0);
+    glVertex3i(90,380,0);
+    glVertex3i(90,300,0);
+    glVertex3i(75,280,0);
+    glEnd();
+
+    glColor3f(1,0,0);
+    glBegin(GL_POLYGON);
+    glVertex3i(130,380,0);
+    glVertex3i(145,360,0);
+    glVertex3i(145,280,0);
+    glVertex3i(130,300,0);
+    glEnd();
+
+    glColor3f(0.8,0.8,0.8);
+    glBegin(GL_POLYGON);
+    glVertex3i(90,375,0);
+    glVertex3i(125,375,0);
+    glVertex3i(125,350,0);
+    glVertex3i(90,350,0);
+    glEnd();
+
+
+    glColor3f(0.8,0.8,0.8);
+    glBegin(GL_POLYGON);
+    glVertex3i(125,375,0);
+    glVertex3i(135,360,0);
+    glVertex3i(135,360,0);
+    glVertex3i(125,350,0);
+    glEnd();
+
+
+    glColor3f(0.8,0.8,0.8);
+    glBegin(GL_POLYGON);
+    glVertex3i(80,360,0);
+    glVertex3i(90,375,0);
+    glVertex3i(90,350,0);
+    glVertex3i(80,355,0);
+    glEnd();
+
+
+
+    glColor3f(1,0,0);
+    glBegin(GL_POLYGON);
+    glVertex3i(130,260,0);
+    glVertex3i(130,310,0);
+    glVertex3i(145,280,0);
+    glVertex3i(145,280,0);
+    glEnd();
+
+    glColor3f(1,0,0);
+    glBegin(GL_POLYGON);
+    glVertex3i(90,260,0);
+    glVertex3i(90,310,0);
+    glVertex3i(75,280,0);
+    glVertex3i(75,280,0);
+    glEnd();
 
 
 
